9b3bf49b38ff14840ea9a3d3b0ad912be5ed4646
Riki <riki@Rikis-MacBook-Pro.local>
2019-02-02 14:13:33 -0500

    "music files and some boiler html"

---
+++ main.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+    var playButton = document.getElementById("playButton");
+    var playing = false;
+    playButton.addEventListener("click", function(){
+        tracks
+        /*
+        if (!playing){
+            var tracks = document.getElementsByClassName("track");
+            for (track in tracks){
+                var aud = track.getElementsByTagName("audio");
+                console.log(aud);
+                aud.play();
+            }
+            playing = true;
+        }
+        else{
+            var tracks = document.getElementsByClassName("track");
+            for (track in tracks){
+                track.audio.stop();
+            }
+            playing = false;
+        }*/
+    });
+    
+};
