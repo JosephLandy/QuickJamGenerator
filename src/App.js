bbba73cf91eb68b04a956e50655c34f0f1b90a32
JosephLandy <joseph.landy.email@gmail.com>
2019-02-02 22:27:41 -0500

    Add audio playing

--- src/App.js
+++ src/App.js
@@ -10,15 +10,25 @@ import padsURL from './music/pads.wav';
 import pianoURL from './music/piano.wav';
 
 import Track from './Track';
+import sound from "./webaudio";
 
 
 // const host = "http://localhost:8000/";
 const backendhost = 'http://127.0.0.1:5000/';
 
 
-const tracks = {
-  arpeg: {URL: arpeggURL, name: 'arpegg', muted: false},
-};
+// const tracks = {
+//   arpeg: {URL: arpeggURL, name: 'arpegg', muted: false},
+// };
+
+const tracksinfo = [
+  {URL: arpeggURL, name: "pegg", mute: false, volume: 50},
+  {URL: bassURL, name: "bass", mute: false, volume: 50},
+  {URL: HiHatURL, name: "HiHat", mute: false, volume: 50},
+  {URL: kickURL, name: "kick", mute: false, volume: 50},
+  {URL: padsURL, name: "pads", mute: false, volume: 50},
+  {URL: pianoURL, name: "Piano", mute: false, volume: 50},
+]
 
 class App extends Component {
 
@@ -27,23 +37,89 @@ class App extends Component {
 
     // this.sample = new Audio(this.url);
     // this.arpegg = new Audio(arpeggURL);
+    this.state = {
+      started: false,
+      tracks: [
+        {URL: arpeggURL, name: "pegg", mute: false, volume: 50},
+        {URL: bassURL, name: "bass", mute: false, volume: 50},
+        {URL: HiHatURL, name: "HiHat", mute: false, volume: 50},
+        {URL: kickURL, name: "kick", mute: false, volume: 50},
+        {URL: padsURL, name: "pads", mute: false, volume: 50},
+        {URL: pianoURL, name: "Piano", mute: false, volume: 50},
+      ],
+
+    };
+
+    this.startHandler = this.startHandler.bind(this);
+    this.muteHandler = this.muteHandler.bind(this);
+
   }
 
+  onComponentDidMount() {
+    // transform state to add audio objects.
+    // this.setState({tracks: tracksinfo.map((elem, index) => {
+    //     return {
+    //       URL: elem.URL,
+    //       name: elem.name,
+    //       mute: elem.mute,
+    //       volume: elem.volume,
+    //       TrackID: index,
+    //       audio: new
+    //
+    //     }
+    //
+    //   })});
+
+  }
+
+  startHandler() {
+    var context = new AudioContext();
+    let sounds = this.state.tracks.map((track) => {
+      new sound(context, new Audio(track.URL), 0);
+    });
+    this.setState({started: true, sounds});
+  }
+
+  muteHandler(index) {
+    if (this.state.tracks[index].mute) {
+      let tracksTemp = [...this.state.tracks];
+      tracksTemp[index] = {...tracksTemp[index], mute: false};
+      this.state.sounds[index].unmute();
+      this.setState({tracks: tracksTemp});
+    } else {
+      let tracksTemp = [...this.state.tracks];
+      tracksTemp[index] = {...tracksTemp[index], mute: true};
+      this.state.sounds[index].mute();
+      this.setState({tracks: tracksTemp});
+
+
+    }
+  }
+
+
   render() {
-    return (
-      <div className="App">
+    if (this.state.started) {
+      return (
+          <div className="App">
+            <div className="container">
+              <Track id="piano" name="Piano" trackID={0} muteHandler={this.muteHandler}/>
+              <Track id="kick" name="Kick" trackID={1} muteHandler={this.muteHandler}/>
+              <Track id="hihat" name="HiHat" trackID={2} muteHandler={this.muteHandler}/>
+              <Track id="bass" name="bass" trackID={3} muteHandler={this.muteHandler}/>
+              <Track id="arpegg" name="arpegg" trackID={4} muteHandler={this.muteHandler}/>
+              <Track id="pads" name="pads" trackID={5} muteHandler={this.muteHandler}/>
+            </div>
+          </div>
+      );
 
-        <div className="container">
-          <Track id="piano" name="Piano" trackID={0}/>
-          <Track id="kick" name="Kick" trackID={1}/>
-          <Track id="hihat" name="HiHat" trackID={2} />
-          <Track id="bass" name="bass" trackID={3} />
-          <Track id="arpegg" name="arpegg" trackID={4} />
-          <Track id="pads" name="pads" trackID={5} />
-        </div>
+    } else {
+      return (
+          <button onClick={this.startHandler}>
+            play
+          </button>
+      );
+    }
 
-      </div>
-    );
   }
 
 }
